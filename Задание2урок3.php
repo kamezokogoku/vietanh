4b44089e958b919f052baee0f2ef676c14a0fc4d
Viet Anh Tran <vietanhtran@Anton.local>
2021-09-29 19:54:24 +0300

    Initial commit

---
+++ Задание2урок3.php
@@ -0,0 +1,96 @@
+
+<?php
+$a = 1;
+switch ($a)
+{
+    case 0:
+        echo ($a++);
+    case 1:
+        echo ($a++);
+    case 2:
+        echo ($a++);
+    case 3:
+        echo ($a++);
+    case 4:
+        echo ($a++);
+    case 5:
+        echo ($a++);
+    case 6:
+        echo ($a++);
+    case 7:
+        echo ($a++);
+    case 8:
+        echo ($a++);
+    case 9:
+        echo ($a++);
+    case 10:
+        echo ($a++);
+    case 11:
+        echo ($a++);
+    case 12:
+        echo ($a++);
+    case 13:
+        echo ($a++);
+    case 14:
+        echo ($a++);
+    case 15:
+        echo ($a++);
+    case 16:
+        echo ($a++);
+    case 17:
+        echo ($a++);
+    case 18:
+        echo ($a++);
+    case 19:
+        echo ($a++);
+    case 20:
+        echo ($a++);
+    case 21:
+        echo ($a++);
+    case 22:
+        echo ($a++);
+    case 23:
+        echo ($a++);
+    case 24:
+        echo ($a++);
+    case 25:
+        echo ($a++);
+    case 26:
+        echo ($a++);
+    case 27:
+        echo ($a++);
+    case 28:
+        echo ($a++);
+    case 29:
+        echo ($a++);
+    case 30:
+        echo ($a++);
+    case 31:
+        echo ($a++);
+    case 32:
+        echo ($a++);
+    case 33:
+        echo ($a++);
+    case 34:
+        echo ($a++);
+    case 35:
+        echo ($a++);
+    case 36:
+        echo ($a++);
+    case 37:
+        echo ($a++);
+    case 38:
+        echo ($a++);
+    case 39:
+        echo ($a++);
+    case 40:
+        echo ($a++);
+    case 41:
+        echo ($a++);
+    case 42:
+        echo ($a++);
+        break;
+    default:
+        echo "меньше 0";
+}
+?>
